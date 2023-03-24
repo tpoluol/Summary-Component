67819ad5e29c33c89eb0cc330c43f7f8f9327d02
DESKTOP-1E9IN30\82104 <tpoluol3@gmail.com>
2023-03-24 16:21:34 +0900

    :wrench: Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-3xl">Hello React.</p>
+    </div>
+  );
+}
+
+export default App;
