4c2bbec78e4d4dcca5faf892191e730bb569f099
tpoluol <109188645+tpoluol@users.noreply.github.com>
2023-03-28 23:56:35 +0900

    :art: Item Component Skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import reaction from '../images/icon-reaction.svg';
+import memory from '../images/icon-memory.svg';
+import verbal from '../images/icon-verbal.svg';
+import visual from '../images/icon-visual.svg';
+import { useState } from 'react';
+import datas from '../data.json';
+
 function App() {
+  const [items] = useState(datas);
   return (
     <div className="flex h-[100vh] items-center justify-center">
       <div className="w-[250px] h-[350px] bg-gradient-to-tl from-LightRoyalBlue to-LightSlateBlue rounded-3xl text-center">
@@ -14,6 +22,27 @@ function App() {
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </div>
+      <div className="p-7 flex-col w-[250px] h-[350px] rounded-3xl shadow-xl">
+        <p>Summary</p>
+        {items.map((itemsa, index) => {
+          return <Item key={index} {...itemsa} />;
+        })}
+        <button className="bg-red-200">Continue</button>
+      </div>
+    </div>
+  );
+}
+
+function Item({ category, score, icon }) {
+  console.log(category);
+  console.log(score);
+  console.log(icon);
+  return (
+    <div className="flex justify-between">
+      <img src={icon} />
+      <p>{category}</p>
+      <p>{score}</p>
+      <p>/ 100</p>
     </div>
   );
 }
